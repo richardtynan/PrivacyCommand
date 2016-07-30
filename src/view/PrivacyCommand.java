c8288c966bddb321d4079bcaef28833c2d385086
Richard Tynan <richie@192.168.101.2>
2016-07-30 17:16:45 +0100

    Initial

--- src/view/PrivacyCommand.java
+++ src/view/PrivacyCommand.java
@@ -5,7 +5,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PrivacyCommand {
